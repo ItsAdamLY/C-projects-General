c86b775da936becc693e315cd81bbca4eae06a29
ItsAdamLY <84637112+ItsAdamLY@users.noreply.github.com>
2021-06-18 01:37:09 +0800

    Add files via upload

---
+++ bonus.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+//constant #define
+#define ARSIZE 5
+int marks[ARSIZE];
+int newScore[ARSIZE];
+// declare dataType method()
+void Edit(int,int [ARSIZE]);
+int GetScore(int, int [ARSIZE]);
+
+int main()
+{
+	int bonus;
+	do {
+	printf("Enter a bonus mark : ");
+	scanf("%d", &bonus);
+	
+	if (bonus < 0 || bonus > 100)
+		printf("Error! Bonus mark must be within range (0-100)\n");
+	} while (bonus < 0 || bonus > 100);
+	
+	Edit(bonus, marks);
+	
+ 	system("pause");
+	return 0;
+}
+
+//method here
+void Edit(int bonus, int marks[ARSIZE])
+{
+	int i;
+	printf("Enter 5 quiz scores : ");
+	
+	for (i=0; i<ARSIZE; i++)
+	{
+		do 
+		{
+			scanf("%d", &marks[i]);
+			if (marks[i] < 0 || marks[i] > 100)
+				printf("Error! Bonus mark must be within range (0-100)\nPlease re-enter score : ");
+		} while (marks[i] < 0 || marks[i] > 100);
+	}
+	
+	newScore[ARSIZE] = GetScore(bonus, marks);
+}
+
+int GetScore(int bonus, int marks[ARSIZE])
+{
+	int i;
+	
+	for (i=0; i<ARSIZE; i++)
+	{	
+		if (marks[i]+bonus > 100)
+			newScore[i] = 100;
+		else	 	
+		newScore[i] = marks[i]+bonus;
+	}
+	
+	printf("The new score array is : ");
+	for (i=0; i<ARSIZE; i++)
+	{		
+		printf("%d ", newScore[i]);
+	}
+	printf("\n");
+}
